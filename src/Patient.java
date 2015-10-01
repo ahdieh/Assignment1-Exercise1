31641a50e060b604d7b162b346bcb3d596f89ba5
ahdieh <aemadi@my.centennialcollege.ca>
2015-09-30 19:34:32 -0700

    Initial commit

---
+++ src/Patient.java
@@ -0,0 +1,85 @@
+
+public class Patient {
+	// PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++
+	private String _patientId;
+	private String _firstName;
+	private String _lastName;
+	private String _address;
+	private String _city;
+	private String _province;
+	private String _postalCode;
+	
+	// PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++++
+		public String getPatientId() {
+			return this._patientId;
+		}
+
+		public void setPatientId(String patientId) {
+			this._patientId = patientId;
+		}
+		
+		public String getFirstName() {
+			return this._firstName;
+		}
+		
+		public void setFirstName(String firstName) {
+			this._firstName = firstName;
+		}
+		
+		public String getLastName() {
+			return this._lastName;
+		}
+		
+		public void setLastName(String lastName) {
+			this._lastName = lastName;
+		}
+		public String getAddress() {
+			return this._address;
+		}
+		
+		public void setAddress(String address) {
+			this._address = address;
+		}
+		public String getCity() {
+			return this._city;
+		}
+		
+		public void setCity(String city) {
+			this._city = city;
+		}
+		public String getProvince() {
+			return this._province;
+		}
+		
+		public void setProvince(String province) {
+			this._province = province;
+		}
+		public String getPostalCode() {
+			return this._postalCode;
+		}
+		
+		public void setPostalCode(String postalCode) {
+			this._postalCode = postalCode;
+		}
+		
+		// CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++
+		public Patient () {
+			this.Initialize("Unknowm", "Unknowm", "Unknowm", "Unknowm", "Unknowm", "Unknowm", "Unknowm");
+		}
+		
+		// PRIVATE METHOD
+		private void Initialize(String patientId, String firstName , String lastName, String address, String city, String province, String postalCode){
+			this.setPatientId(patientId);
+			this.setFirstName(firstName);
+			this.setLastName(lastName);
+			this.setAddress(address);
+			this.setCity(city);
+			this.setProvince(province);
+			this.setPostalCode(postalCode);
+				
+		}
+		
+		public void getPatientInfo() {
+			System.out.printf("%s %s Patient Information: \n", getFirstName(), getLastName());
+		}
+}
---
+++ src/PatientTest.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+public  class PatientTest  {
+	public static Patient patient; // reference to the Patient class
+	public static void main(String[] args) {
+		System.out.println("Program Started...");
+			Scanner input = new Scanner(System.in);
+		
+		// Prompt user for Patient Id
+	    System.out.print("Enter your Patient Id: ");
+		String myPatientId= input.nextLine();
+		
+		// Prompt user for First Name
+		System.out.print("Enter your First Name: ");
+		String myFirstName = input.nextLine();
+		System.out.print("Enter your Last Name: ");
+		String myLastName = input.nextLine();
+		
+		// Prompt user for Address
+		System.out.print("Enter your Address: ");
+		String myAddress = input.nextLine();
+		
+		// Prompt user for City
+		System.out.print("Enter your City: ");
+		String myCity = input.nextLine();
+				
+		// Prompt user for Province
+		System.out.print("Enter your Province: ");
+	    String myProvince = input.nextLine();
+				
+		// Prompt user for Postal Code
+		System.out.print("Enter your Postal Code: ");
+		String myPostalCode = input.nextLine();
+				
+		
+		
+		patient = new Patient();
+		
+		patient.setPatientId(myPatientId);
+		patient.setFirstName(myFirstName);
+		patient.setLastName(myLastName);
+		patient.setAddress(myAddress);
+		patient.setCity(myCity);
+		patient.setProvince(myProvince);
+		patient.setPostalCode(myPostalCode);
+		
+		patient.getPatientInfo();
+		
+		System.out.printf("The patient %s %s with ID#%s lives in %s , %s ,%s ,%s", patient.getFirstName(), patient.getLastName(), patient.getPatientId(), patient.getAddress(), patient.getCity(), patient.getProvince(), patient.getPostalCode());
+		
+	}
+
+}
